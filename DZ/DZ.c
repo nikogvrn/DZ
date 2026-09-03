5249c7a3900729897004cea6bc2e9cf05d6e6306
nikogvrn <nikogvrn@gmail.com>
2026-09-03 21:45:02 +0300

    Добавьте файлы проекта.

---
+++ DZ/DZ.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	puts("    |     |");
+	puts("    |     |");
+	puts("  /--------\\");
+	puts(" /    12    \\");
+	puts(" | 9        3|");
+	puts(" \\    6      /");
+	puts("  \\---------/");
+	puts("    |     |");
+	puts("    |     |");
+	return 0;
+}
